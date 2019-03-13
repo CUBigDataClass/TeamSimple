49c7a3b19e2e80f6f846f13ff2422495960e30b1
knights207210 <xuha2442@colorado.edu>
2019-03-12 21:49:30 -0600

    kafkatry

--- kafka_xu/script.py
+++ kafka_xu/script.py
@@ -1,1 +1,3 @@
 zookeeper-server-start /usr/local/etc/kafka/zookeeper.properties & kafka-server-start /usr/local/etc/kafka/server.properties
+zookeeper-server-stop
+kafka-server-stop
--- kafka_xu/try.py
+++
@@ -1,4 +0,0 @@
-config = {}
-exec(compile(open("config.py", "rb").read(), "config.py", 'exec'), config)
-#execfile("config.py", config)
-print(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"])
