a7e160cf29943c585c0082476e4b561c66cadfaa
knights207210 <xuha2442@colorado.edu>
2019-03-16 20:34:06 -0700

    script

--- kafka_xu/script.py
+++ kafka_xu/script.py
@@ -1,3 +1,9 @@
 zookeeper-server-start /usr/local/etc/kafka/zookeeper.properties & kafka-server-start /usr/local/etc/kafka/server.properties
 zookeeper-server-stop
 kafka-server-stop 
+
+
+/usr/libexec/java_home
+
+
+/usr/local/Cellar/hadoop/hdfs/tmp/dfs/name;
