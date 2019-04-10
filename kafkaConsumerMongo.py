c63e95a5887d87dd5dd16620b30d2db7c6f00b88
dongyao31 <dowa4431@colorado.edu>
2019-04-09 20:49:39 -0600

    parse emoji and store them into separate message dic then store into database

--- kafkaConsumerMongo.py
+++ kafkaConsumerMongo.py
@@ -1,36 +1,38 @@
-
 # coding: utf-8
-
-# In[4]:
-
 
 import threading, logging, time, json,pymongo
 from kafka import KafkaConsumer, KafkaProducer
 from pymongo import MongoClient
 import json
 
+from emoji_filter import *
 
-# In[5]:
 
-
+print("Creating mogo connection...")
 client = MongoClient('localhost', 27017)
 mydb = client['mydatabase']
 mycollection = mydb['tweets_test']
+
+
+print("Creating consumer...")
 consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
                          auto_offset_reset='earliest',
                          group_id='mongo'
                         )
+
+
+print("Subscribing...")
 consumer.subscribe('tweepy-kafka-test1')
 
 
-# In[6]:
-
-
+print("For loop(listening)...")
 for msg in consumer:
-    #print(msg.value)
-    byteText = msg.value   
-    text = str(byteText,"utf-8")
-    #text = json
-    data_to_insert = json.loads(text, strict=False)
-    x = mycollection.insert_one(data_to_insert)
-
+	#print(msg.value)
+	byteText = msg.value
+	text = str(byteText,"utf-8")
+	#text = json
+	data_to_insert = json.loads(text, strict=False)
+	
+	for tweet_dic in parse_emoji_and_divide_into_seperate_messages(data_to_insert):
+		print(tweet_dic)
+		x = mycollection.insert_one(tweet_dic)
