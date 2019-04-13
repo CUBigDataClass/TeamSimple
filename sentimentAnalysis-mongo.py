8afd28b867139f90ce0154ed0d3bc68e79f68ebd
wyc0807 <wangyichen5151@gmail.com>
2019-04-13 15:49:15 -0600

    update sentiment analysis

--- sentimentAnalysis-mongo.py
+++ sentimentAnalysis-mongo.py
@@ -1,9 +1,4 @@
-
 # coding: utf-8
-
-# In[58]:
-
-
 from pyspark.sql.functions import udf, col,split
 from pyspark.ml.clustering import KMeans
 import json
@@ -21,10 +16,6 @@ import sys
 import shutil
 import nltk
 from pyspark import SparkConf, SparkContext
-
-
-# In[56]:
-
 
 def update_sentimentAndEmoji_counts_using_tweets(tweets_collection, tweets_with_sentiment_collection, emoji_collection, numberOfTweetsToParse):
     # How many emoji we want to grab from source collection.
@@ -86,10 +77,6 @@ def update_sentimentAndEmoji_counts_using_tweets(tweets_collection, tweets_with_
             number_of_tweets_parsed +=1
         highest_previous_primary_key = current_primary_key
 
-
-# In[57]:
-
-
 # Create database conncetion.
 print("Creating mongo connection...")
 client = MongoClient('localhost', 27017)
@@ -106,129 +93,3 @@ while(True):
     # Grab tweets from tweets_collection and store emoji counts into emoji_collection.
     update_sentimentAndEmoji_counts_using_tweets(tweets_collection, tweets_with_sentiment_collection, emoji_collection,numberOfTweetsToParse)
     time.sleep(1)
-
-
-# In[ ]:
-
-
-highest_previous_primary_key = 0
-scores = []
-while True:
-    cursor = mycollection.find()
-    count = 0
-    for document in cursor:
-        # get the current primary key, and if it's greater than the previous one
-        # we print the results and increment the variable to that value
-        current_primary_key = int('0x'+str(document['_id'])[-6:],16)
-        if current_primary_key > highest_previous_primary_key:
-            #do your work only on newly incoming rows
-            tweet = document["text"]
-            text = re.sub(r"http\S+", "", tweet)
-            if text == '':
-                mycollection.update({"_id": document["_id"]}, {"$set": {"sentiment_score_text": 0}})
-                continue            
-            blob = TextBlob(text)
-            score = []
-            for sentence in blob.sentences:
-                score.append(sentence.sentiment.polarity)
-            sent_score = sum(score)/len(score)
-            #scores.append(sent_score)
-            mycollection.update({"_id": document["_id"]}, {"$set": {"sentiment_score_text": sent_score}})
-            #count += 1
-        highest_previous_primary_key = current_primary_key
-    #print(count)
-    time.sleep(3)
-
-
-# In[2]:
-
-
-from pyspark.sql import SparkSession
-my_spark = SparkSession     .builder     .appName("myApp")     .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/mydatabase.tweets_test")     .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/mydatabase.tweets_test")     .config("spark.io.compression.codec", "snappy").getOrCreate() #this line's config is for solving lz4 error
-dataFrame=my_spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
-dataFrame.printSchema()
-
-
-# In[3]:
-
-
-#split = udf(lambda x: x.split(','))
-#df.withColumn("user_location", split_udf(col("user_location"))).show()
-#df=dataFrame.withColumn("user_location",
-   # split(col("user_location"), ",\s*").cast("array<float>").alias("user_location")
-#)
-#df_loc = df.select('user_location')
-
-
-# In[4]:
-
-
-from pyspark.ml.linalg import Vectors
-from pyspark.ml.feature import VectorAssembler
-assembler = VectorAssembler(
-    inputCols=['loc_lat','loc_long'],
-    outputCol='features')
-trainingData = assembler.transform(dataFrame)
-#trainingData.show()
-
-
-# In[5]:
-
-
-kmeans = KMeans(k=3,seed=1)
-model = kmeans.fit(trainingData)
-# Shows the result.
-centers = model.clusterCenters()
-print("Cluster Centers: ")
-for center in centers:
-    print(center)
-
-
-# In[6]:
-
-
-cluster_ind = model.transform(trainingData)
-dataToKibana = cluster_ind.toPandas().to_dict('record')
-for item in dataToKibana:
-    item['created_at'] =item['created_at'][:10]+'T'+item['created_at'][11:]+'Z'
-    
-for item in dataToKibana:
-    item['loc_lat'] =round(item['loc_lat'],2)
-    item['loc_long'] =round(item['loc_long'],2)
-
-
-# In[9]:
-
-
-es = Elasticsearch(hosts='http://localhost',port=9200)
-actions = []
-mappings = {
-    "mappings":{
-        "tweet": {
-            "properties": {
-                "text": { "type": "text"  },
-                "timestamp": { "type": "date" },
-                "location": {"type": "geo_point"},
-                "prediction": {"type": "integer"}
-                }
-            }
-    }
-}
-es.indices.create(index="loc11", body=mappings)
-for msg in dataToKibana:
-    print(msg["text"])
-    print("-------------")
-    action = {
-            "index": "loc6",
-            "type": "tweet",
-            "source": {
-                'text' : msg["text"],
-                'timestamp': msg["created_at"],
-                'location': {"lat": msg["loc_lat"],"lon": msg["loc_long"]},
-                'prediction': msg["prediction"]
-                }
-            }
-    actions.append(json.dumps(action))
-helpers.bulk(es, actions, index='loc11', doc_type='tweet')
-    
-
