1e8669195a5fa184139b96d6a1cc8ad01bd6fe8a
wyc0807 <wangyichen5151@gmail.com>
2019-04-19 16:15:52 -0600

    clear some codes

--- sentimentAnalysis-mongo.py
+++ sentimentAnalysis-mongo.py
@@ -32,14 +32,16 @@ def update_sentimentAndEmoji_counts_using_tweets(tweets_collection, tweets_with_
                 tweet = document["text"]
                 text = re.sub(r"http\S+", "", tweet)
                 if text == '':
-                    tweets_collection.update({"_id": document["_id"]}, {"$set": {"sentiment_score_text": 0}})
+                    tweets_collection.update({"_id": document["_id"]}, {"$set": {"sentimentScoreText": 0}})
                     continue            
                 blob = TextBlob(text)
                 sentScore_list = []
                 for sentence in blob.sentences:
                     sentScore_list.append(sentence.sentiment.polarity)
                 sent_score = sum(sentScore_list)/len(sentScore_list)
-                tweets_collection.update({"_id": document["_id"]}, {"$set": {"sentiment_score_text": sent_score}})
+                tweets_collection.update({"_id": document["_id"]}, {"$set": {"sentimentScoreText": sent_score}})
+            
+
             
                 # Get emojis from current tweet.
                 emojis = extract_emojis(document['text'])
@@ -52,27 +54,27 @@ def update_sentimentAndEmoji_counts_using_tweets(tweets_collection, tweets_with_
                 del scores['compound']
                 sentiment = max(scores, key=scores.get)
                 print(emojis_str, sentiment)
-                document['emojis'] = emojis_str
-                #document['sentiment_score_text'] = sent_score
-                document['sentiment_emoji'] = sentiment
+                document['emoji'] = emojis_str
+                #document['sentimentScoreText'] = sent_score
+                document['sentimentEmoji'] = sentiment
                 document.pop('_id', None)
                 document.pop('text', None)
                 # Append emojis str and sentiment result to current entry and store it to a new dic
                 tweets_with_sentiment_collection.insert_one(document)    
 
                 ''' ----------  Count emojis summary and store them into a new collection ----------'''
-                for emoji in emojis:
-                    emoji = emoji.strip() #remove spaces. unlikely....
-                    # Look for current emoji in target collection.
-                    result = emoji_collection.find_one({'emoji':emoji}) 
-                    # First time see this emoji. Initialize it and set count to 1.
-                    if result == None:
-                        #print("Initializing "+emoji)
-                        emoji_collection.insert_one({'emoji':emoji, 'count':1})    
-                    # Emoji already exists in collection. Increment count by 1.
-                    else:
-                        #print("Updating "+emoji)
-                        emoji_collection.update_one(result, {"$set":{'count':int(result['count'])+1}})
+                # for emoji in emojis:
+                #     emoji = emoji.strip() #remove spaces. unlikely....
+                #     # Look for current emoji in target collection.
+                #     result = emoji_collection.find_one({'emoji':emoji}) 
+                #     # First time see this emoji. Initialize it and set count to 1.
+                #     if result == None:
+                #         #print("Initializing "+emoji)
+                #         emoji_collection.insert_one({'emoji':emoji, 'count':1})    
+                #     # Emoji already exists in collection. Increment count by 1.
+                #     else:
+                #         #print("Updating "+emoji)
+                #         emoji_collection.update_one(result, {"$set":{'count':int(result['count'])+1}})
                 # How many tweets we want to grab from source collection.
                 #if number_of_tweets_parsed > numberOfTweetsToParse:
                 #    break
@@ -87,7 +89,7 @@ mydb = client['mydatabase']
 # Source collection to read from.
 tweets_collection = mydb['tweets_test']
 # Collection to store sentiment and emoji count
-tweets_with_sentiment_collection = mydb['tweets_with_sentiment_test']
+tweets_with_sentiment_collection = mydb['tweets_emoji_entiment']
 emoji_collection = mydb['emojis_test'] 
 # How many emoji we want to grab from source collection each round. only for testing
 numberOfTweetsToParse = 500
