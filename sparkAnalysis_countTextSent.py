21e172a9f7112c1c3ad8b3a81a259d6be455ab82
wyc0807 <wangyichen5151@gmail.com>
2019-04-20 17:53:59 -0600

    update spark analysis

--- sparkAnalysis_countTextSent.py
+++ sparkAnalysis_countTextSent.py
@@ -8,9 +8,8 @@ while True:
     .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/mydatabase.tweets_text_sentiment") \
     .config("spark.io.compression.codec", "snappy").getOrCreate() #this line's config is for solving lz4 error
     df=my_spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
-    emojiTypeCount = df.groupBy("sentimentScoreText").count().sort('count',ascending=False)
-    maxCount = emojiTypeCount.limit(1)
-    maxCount.write.format("com.mongodb.spark.sql.DefaultSource").mode("overwrite").option("database",
+    sentCount = df.groupBy("sentimentScoreText").count().sort('count',ascending=False)
+    sentCount.write.format("com.mongodb.spark.sql.DefaultSource").mode("overwrite").option("database",
     	"mydatabase").option("collection", "text_sentiment_count").save()
 
 
